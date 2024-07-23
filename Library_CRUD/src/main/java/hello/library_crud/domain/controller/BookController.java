9bfccc6b14953efaa380542e36b309d6dd3d8a8c
bhyun <nbhyun0329@gmail.com>
2024-07-23 12:23:53 +0900

    ✨ FEAT :: 도서 등록 기능 추가
    도서의 이름, 저자, 출간일을 입력하고 등록할 수 있는 기능 추가

--- Library_CRUD/src/main/java/hello/library_crud/domain/controller/BookController.java
+++ Library_CRUD/src/main/java/hello/library_crud/domain/controller/BookController.java
@@ -1,2 +1,18 @@
-package hello.library_crud.domain.controller;public class BookController {
+package hello.library_crud.domain.controller;
+
+import hello.library_crud.domain.dto.request.RegisterRequest;
+import hello.library_crud.domain.service.BookRegisterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+    private final BookRegisterService bookRegisterService;
+
+    public void register(RegisterRequest registerRequest) {
+        bookRegisterService.register(registerRequest);
+    }
+
+    
 }
