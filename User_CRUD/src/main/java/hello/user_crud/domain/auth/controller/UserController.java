aa2402b8ce18e6df0ed2b5abb60ddc8f0979b78f
bhyun <nbhyun0329@gmail.com>
2024-07-24 15:16:56 +0900

    ✨ FEAT :: 유저 조회 서비스 추가
    고유 id로 유저를 조회해서 유저 이름, 이메일, 전화번호, 생일을 조회하도록 기능 추가

--- User_CRUD/src/main/java/hello/user_crud/domain/auth/controller/UserController.java
+++ User_CRUD/src/main/java/hello/user_crud/domain/auth/controller/UserController.java
@@ -1,9 +1,12 @@
 package hello.user_crud.domain.auth.controller;
 
+import hello.user_crud.domain.auth.dto.request.CheckRequest;
 import hello.user_crud.domain.auth.dto.request.SignUpRequest;
+import hello.user_crud.domain.auth.dto.response.CheckResponse;
 import hello.user_crud.domain.auth.service.CheckService;
 import hello.user_crud.domain.auth.service.SignUpService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,10 @@ public class UserController {
         signUpService.signUp(signUpRequest);
     }
 
-    @
+    @GetMapping("/check")
+    public CheckResponse check(@RequestBody CheckRequest checkRequest) {
+        return checkService.check(checkRequest);
+    }
+    
+
 }
