4cd49e64b074fe2cb7c55a9a03312a83762b9d5c
bhyun <nbhyun0329@gmail.com>
2024-07-25 09:18:22 +0900

    ✨ FEAT :: 엔티티 생성
    상품 엔티티 생성

---
+++ Shop_CRUD/src/main/java/hello/shop_crud/domain/goods/entity/GoodsEntity.java
@@ -0,0 +1,26 @@
+package hello.shop_crud.domain.goods.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GoodsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long GoodsId;
+
+    @Column(nullable = false)
+    private String goodsName;
+
+    @Column(nullable = false)
+    private String goodsDesc;
+
+    @Column(nullable = false)
+    private String goodsExplan;
+}
