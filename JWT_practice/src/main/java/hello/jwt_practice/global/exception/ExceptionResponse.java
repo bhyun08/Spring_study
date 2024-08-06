74c2a729e9685b9e83efcb4d520e1349e5deec25
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-08-06 17:15:03 +0900

    Delete JWT_practice directory

--- JWT_practice/src/main/java/hello/jwt_practice/global/exception/ExceptionResponse.java
+++
@@ -1,9 +0,0 @@
-package hello.jwt_practice.global.exception;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class ExceptionResponse {
-    public int status;
-    public String message;
-}
